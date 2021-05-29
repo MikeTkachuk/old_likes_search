b85d18d84f10adfe23029b58826f17c77eca696a
MikeTkachuk <mykhail.tkachuk@gmail.com>
2021-05-29 21:52:07 +0200

    real base?

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import os
 import flask
-import time
-import numpy as np
+import sys
+import oauth2 as oauth
 import tweepy as tw
 from tweepy.auth import OAuthHandler
 from tweepy.api import API
 
-# auth = OAuthHandler()
 
 app = flask.Flask('likes_search')
 
 
 @app.route('/')
 def render_index():
-    flask.render_template('index.html')
+    return flask.render_template('index.html')
 
 
 app.run()
