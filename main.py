607ddd04d2f1e88622af911e3dd8f779edf9b9c1
MikeTkachuk <mykhail.tkachuk@gmail.com>
2021-05-29 13:33:18 +0200

    test py flask server

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import flask
+import time
+import numpy as np
+import tweepy as tw
+from tweepy.auth import OAuthHandler
+from tweepy.api import API
+
+# auth = OAuthHandler()
+
+app = flask.Flask('likes_search')
+
+
+@app.route('/')
+def render_index():
+    flask.render_template('index.html')
--- script/main.js
+++ script/main.js
@@ -1,3 +1,6 @@
 function main(){
   document.getElementById("p1").innerHTML = "something"
 }
+
+var oauth = require('node-oauth');
+alert(oauth);
