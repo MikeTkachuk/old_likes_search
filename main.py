a671910addfe76024222908d7fa76f323085f25c
MikeTkachuk <mykhail.tkachuk@gmail.com>
2021-05-30 07:55:52 +0200

    r

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import flask
 
 
-app = flask.Flask('likes_search')
+app = flask.Flask(__name__)
 
 
 @app.route('/')
@@ -9,4 +9,5 @@ def render_index():
     return flask.render_template('index.html')
 
 
-app.run()
+if __name__ == '__main__':
+    app.run()
