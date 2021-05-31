68085a5832a30c9a3bd619ede0f1abcb639f0f80
MikeTkachuk <mykhail.tkachuk@gmail.com>
2021-05-31 16:59:54 +0200

    Update extend.js

--- static/scripts/extend.js
+++ static/scripts/extend.js
@@ -1,9 +1,12 @@
+count_extensions = 0;
 function handle_extend(){
   let re = new XMLHttpRequest();
   re.open('get', '/query/extend');
   re.send();
   re.onload = () => {
   var text = re.responseText;
-  document.getElementById("results").innerHTML += text;
+  document.getElementById("results").innerHTML +='<div id="results_'+toString(count_extensions)+'">' + text + '</div>';
+   count_extensions += 1;
+    twittr.widgets.load(document.getElementById("results_"+toString(count_extensions)));
   }
 }
