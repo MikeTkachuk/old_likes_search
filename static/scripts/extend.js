c55d14fe7bb089a608d125fcc9ca9c1fdb51705e
MikeTkachuk <mykhail.tkachuk@gmail.com>
2021-05-31 18:01:39 +0200

    Update extend.js

--- static/scripts/extend.js
+++ static/scripts/extend.js
@@ -6,7 +6,7 @@ function handle_extend(){
   re.onload = () => {
   var red = JSON.parse(re.responseText);
   for (tweet in red){
-     document.getElementById("results").innerHTML +='<div id="results_'+count_extensions.toString()+'">' + tweet + '</div>';
+     document.getElementById("results").innerHTML +='<div id="results_'+count_extensions.toString()+'">' + red[tweet] + '</div>';
     twttr.widgets.load(document.getElementById("results_"+count_extensions.toString()));
     count_extensions += 1;
   }
