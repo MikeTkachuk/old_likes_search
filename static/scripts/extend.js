d52e097d3fee759b3a46e88012472ea8ea4f4a0f
MikeTkachuk <mykhail.tkachuk@gmail.com>
2021-06-02 14:46:06 +0200

    Update extend.js

--- static/scripts/extend.js
+++ static/scripts/extend.js
@@ -15,32 +15,32 @@ async function render_extended(tweets){
     url.searchParams.append("url",tweets[i]);
     url.searchParams.append("omit_script","true");
     url = url.href;
-    await sub_request_handler(url,count_extensions);
+    sub_request_handler(url,count_extensions);
     count_extensions++;
   }
+ await twttr.widgets.load(document.getElementById('results'));
+ 
 }
 
-async function sub_request_handler(link,order){
+function sub_request_handler(link,order){
   let re_url = new URL("https://old-likes-search.herokuapp.com/get_tweet_html");
     re_url.searchParams.append("url",link);
     let re_html  = new XMLHttpRequest();
     re_html.open('get',re_url.href);
     re_html.send();
-    re_html.onload = async () => {
+    re_html.onload = () => {
       let red = JSON.parse(re_html.responseText)["html"];
       set_div(order,red);
-     await render_tweet(order);
     }
 }
 
 function set_div(order,content){
-document.getElementById("results").innerHTML += 
-    '<div id="results_'+order.toString()+'">'+content+'</div>';
+document.getElementById("results").innerHTML += content;
     console.log("added div with id 'results_'"+order.toString());
       
 }
 
 async function render_tweet(order){
- twttr.ready(()=>{twttr.widgets.load(document.getElementById("results_"+order.toString()));});  
+ await twttr.widgets.load(document.getElementById("results_"+order.toString()));  
  console.log("loaded tweet "+order.toString());
 } 
