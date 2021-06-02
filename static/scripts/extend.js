667471dc9eba3b90fd43736dda5d35476b81376e
MikeTkachuk <mykhail.tkachuk@gmail.com>
2021-06-02 17:12:44 +0200

    try1.0

--- static/scripts/extend.js
+++ static/scripts/extend.js
@@ -17,7 +17,7 @@ function handle_extend(){
   re_links.open('get', '/query/extend');
   re_links.send();
   re_links.onload = async() => {
-    document.getElementById('results').innerHTML += await set_html('https://twitter.com/RussianMemesLtd/status/1400026144779517964');
+    document.getElementById('results').innerHTML += await set_html('https://publish.twitter.com/oembed?url=https%3A%2F%2Ftwitter.com%2FRussianMemesLtd%2Fstatus%2F1400026144779517964');
     document.getElementById("results").innerHTML += '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Tomday you don&#39;t shit <a href="https://t.co/RzPQarCgXe">pic.twitter.com/RzPQarCgXe</a></p>&mdash; Russian Memes United (@RussianMemesLtd) <a href="https://twitter.com/RussianMemesLtd/status/1400026144779517964?ref_src=twsrc%5Etfw">June 2, 2021</a></blockquote>';
   twttr.widgets.load(document.getElementById("results"));
   }
