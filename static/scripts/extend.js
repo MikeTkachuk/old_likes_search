a9aad9181db7ff7b786651d4b23f654e717fbf25
MikeTkachuk <mykhail.tkachuk@gmail.com>
2021-06-03 18:46:51 +0200

    Update extend.js

--- static/scripts/extend.js
+++ static/scripts/extend.js
@@ -6,13 +6,12 @@ async function get_html(link,order){
   param_url.searchParams.append('url',link);
   param_url.searchParams.append('omit_script','true');
   re_url.searchParams.append("url",param_url.href);
-  console.log(re_url.href);
   out = await fetch(re_url.href,{method:"GET"});
   return out.json().then(
-    res=>{
+    async(res)=>{
       document.getElementById('results').innerHTML += '<div id="result_'+order.toString()+'">'+
        res['html']+ '</div>';
-      twttr.widgets.load(document.getElementById("result_"+order.toString()));
+      return twttr.widgets.load(document.getElementById("result_"+order.toString()));
 
     }
     );
@@ -23,9 +22,11 @@ function handle_extend(){
   re_links.send();
   re_links.onload = async() => {
     let links = JSON.parse(re_links.responseText);
-    for(i=0;i<links.length;i++){
+    await get_html(links[0],0);
+    await get_html(links[1],1);
+    /*for(i=1;i<links.length;i++){
       await get_html(links[i],count_extensions);
       count_extensions++;
-    }
+    }*/
   }
 } 
